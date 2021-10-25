f84bdd8697750667824d68ed02d7dd5e46f5a1f0
ADDIN97 <pawebak58@gmail.com>
2021-10-25 10:00:49 +0200

    Initial Commit
    Initial Commit

---
+++ WEBSITE_BLOCKER/main.py
@@ -0,0 +1,27 @@
+#WEBSITE BLOCKER SCRIPT
+import time
+from datetime import datetime as dt
+host_temp = "C:\\Users\\Paweł\\Desktop\\hosts"
+redirect="127.0.0.1"
+website_list=["www.onet.pl","onet.pl","www.wp.pl","wp.pl"]
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+        print("Working hours...")
+        with open(host_temp,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+ website+"\n")
+    else:
+        with open(host_temp,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("Fun hours...")
+    time.sleep(5)
